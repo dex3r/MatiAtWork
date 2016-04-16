035f24dfa006c15cf3b05244520e18b751d644cd
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2016-04-16 20:33:35 +0200

    Cleanup

--- Game2048WebView/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Game2048WebView.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Game2048WebView/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Game2048WebView.Global" Language="C#" %>
